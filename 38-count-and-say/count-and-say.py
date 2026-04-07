acc0ba4f858d638634717d3bbde77cc902e9964e
AveshKadari47 <kadariavesh@gmail.com>
2026-04-07 22:53:30 +0530

    Time: 7 ms (93.63%) | Memory: 12.6 MB (38.63%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        result = "1"
+        
+        for _ in range(n - 1):
+            temp = ""
+            count = 1
+            
+            for i in range(1, len(result)):
+                if result[i] == result[i - 1]:
+                    count += 1
+                else:
+                    temp += str(count) + result[i - 1]
+                    count = 1
+            
+            # Add the last group
+            temp += str(count) + result[-1]
+            result = temp
+        
+        return result
